c08629750518af41c6dfee88938326fcc72c5d4b
xsm10202713 <xsm10202713@gmail.com>
2021-07-12 01:28:28 +0800

    是

--- Test.java
+++ Test.java
@@ -1,7 +1,6 @@
-/**
- * Test
- */
-public class Test {
-
-    
+public class Test{
+    public static void main(String[] args){
+        System.out.println("helloo world");
+        System.out.println("hello world");
+    }
 }
